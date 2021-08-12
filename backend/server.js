3917d665881bb7f83b46850d134f3c7f161efa02
asif <islamasif570@gmail.com>
2021-08-12 16:14:31 +0600

    inastall and config nodemon

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,13 @@ import {products} from './data.js'
 const app = express();
 
 app.get('/' , (req , resp) =>{ 
-    resp.send("server is ready");
+    resp.send("server is ready 2");
 });
 
 app.get('/api/products' , (req , resp) =>{ 
     resp.send(products);
 });
-
-app.listen(5000 , () =>{
-    console.log( "Server is http://localhost:5000");
+ const port = process.env.PORT || 5000;
+app.listen(port , () =>{
+    console.log( "Server is http://localhost:${port}");
 });
